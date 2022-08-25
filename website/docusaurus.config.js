8e79710c4322802a8d3970aeadd0ff26a931da2b
Edgar Riba <edgar.riba@gmail.com>
2022-08-25 06:21:05 +0200

    [docs]  fix baseUrl to /amiga-dev-kit/ (#9)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Farm-ng Developers',
   tagline: 'Robots are cool',
   url: 'https://farm-ng.com',
-  baseUrl: '/',
+  baseUrl: '/amiga-dev-kit/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/farm-ng_favicon.png',
