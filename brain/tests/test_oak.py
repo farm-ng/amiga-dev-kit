3f3114826072fe605a7c9ebee14bf6e7daa427ec
Edgar Riba <edgar.riba@gmail.com>
2022-09-01 10:55:55 +0200

    fix circular imports and ci (#22)
    * comment __init__ imports
    
    * reorder flake in pre-commit
    
    * move pyproject.toml
    
    * test black
    
    * debug ci
    
    * fix

--- brain/tests/test_oak.py
+++ brain/tests/test_oak.py
@@ -1,5 +1,5 @@
 import pytest
-from farm_ng.oak import OakCameraClient, OakCameraClientConfig, OakCameraServiceState, oak_pb2
+from farm_ng.oak.client import OakCameraClient, OakCameraClientConfig, OakCameraServiceState, oak_pb2
 
 
 @pytest.fixture
