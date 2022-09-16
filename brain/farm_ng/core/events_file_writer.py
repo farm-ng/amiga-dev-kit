adb843a0778ca5168e6dc8f52072fb85b0b21038
edgar <edgar.riba@gmail.com>
2022-09-16 17:24:12 +0200

    add basic tests for reader/writer

--- brain/farm_ng/core/events_file_writer.py
+++ brain/farm_ng/core/events_file_writer.py
@@ -21,7 +21,7 @@ class EventsFileWriter:
     def file_name(self) -> Optional[Path]:
         if self._file_stream is None:
             return None
-        return self._file_stream.name
+        return Path(self._file_stream.name)
 
     def is_open(self) -> bool:
         return not self.is_closed()
