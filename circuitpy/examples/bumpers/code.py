991fcd7414e30bf8b44eba2e2cfedce3bb44a058
Gary Bradski <garybradski@gmail.com>
2023-02-28 11:10:09 -0800

    correcting pre-commits

--- circuitpy/examples/bumpers/code.py
+++ circuitpy/examples/bumpers/code.py
@@ -1,13 +1,14 @@
-'''
-This class reads bumper contacts where the 3 volt pin on the feather chip is connected to a
+"""This class reads bumper contacts where the 3 volt pin on the feather chip is connected to a
 bumper contact connecting the 3 volts to bumpers attached to pins D10-D13 driving them high
 if the bumper is pressed.
 For CAN Bus, message ID is BUMPER_NODE_ID = 0x2F (see corresponding packet.py)
 4 shorts (8 bytes) are sent as the CAN bus message. The lower order bits are set
 where "1" means "bumper pressed" So pins are bit coded in the first 4 bits
        bit 0 => pin D10, bit 1 => pin D11, bit 2 => pin D12, bit 3 => pin D13
-'''
-
+"""
+import board
+import digitalio
+import adafruit_debouncer
 from canio import Message
 from farm_ng.utils.cobid import CanOpenObject
 from farm_ng.utils.general import TickRepeater
@@ -15,14 +16,9 @@ from farm_ng.utils.main_loop import MainLoop
 from farm_ng.utils.packet import BUMPER_NODE_ID
 from farm_ng.utils.packet import BumperState
 from usb_cdc import console
-
-import board
-import digitalio
 # NOTE: To use debouncer, you may have to clone farm_ng firmware: amiga-fw and get 2 files from
 # amiga-fw/thirdparty: adafruit_debouncer.mpy and adafruit_ticks.mpy. Copy these into the adafruit's
 # lib directory (code.py goes to the root directory):
-import adafruit_debouncer
-
 
 def read_adafruit_pin(p):
     '''
@@ -32,8 +28,8 @@ def read_adafruit_pin(p):
     '''
     r = digitalio.DigitalInOut(p)
     r.direction = digitalio.Direction.INPUT
-    r.pull = digitalio.Pull.DOWN  #Bumper contact is a high state, so pull down first to detect "up"
-    return adafruit_debouncer.Debouncer(r, interval=0.05) #Set for contact 1/20th second to set
+    r.pull = digitalio.Pull.DOWN  # Bumper contact is a high state, so pull down first to detect "up"
+    return adafruit_debouncer.Debouncer(r, interval=0.05)  # Set for contact 1/20th second to set
 
 
 class BumperMainLoopApp:
@@ -57,11 +53,11 @@ class BumperMainLoopApp:
 
     def _register_message_handlers(self):
         # FILL IN IF YOU WANT BUMPERS TO HANDLE MESSAGES
-        #self.main_loop.command_handlers[CanOpenObject.TPDO1 | DASHBOARD_NODE_ID] = self._handle_amiga_tpdo1
+        # self.main_loop.command_handlers[CanOpenObject.TPDO1 | DASHBOARD_NODE_ID] = self._handle_amiga_tpdo1
         pass
 
     def _handle_bumpers(self, message):
-        '''FILL IN THIS IF YOU WANT BUMPERS TO HANDLE MESSAGES'''
+        """FILL IN THIS IF YOU WANT BUMPERS TO HANDLE MESSAGES"""
         pass
 
     def iter(self):
@@ -80,12 +76,8 @@ class BumperMainLoopApp:
             bbytes = (0x1 * D10) + (0x2 * D11) + (0x4 * D12) + (0x8 * D13)
             bumper_state_obj = BumperState(bbytes)
             # NOTE: IF DEVELOPING ON LAPTOP, PRINT BUMPER STATE MESSAGE:
-            #print("bumperstate {}".format(bumper_state_obj))
-            self.can.send(
-                Message(
-                    id=CanOpenObject.TPDO1 | BUMPER_NODE_ID,
-                    data=bumper_state_obj.encode())
-                )
+            # print("bumperstate {}".format(bumper_state_obj))
+            self.can.send(Message(id=CanOpenObject.TPDO1 | BUMPER_NODE_ID, data=bumper_state_obj.encode()))
 
 
 def main():
