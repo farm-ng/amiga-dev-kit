08aed23e99c478a8323b86c4734bf844ddc22c1c
Gui <guilhermedemouraa@gmail.com>
2023-12-08 12:01:46 -0800

    Camera App Overview

--- website/sidebars.js
+++ website/sidebars.js
@@ -134,6 +134,7 @@ const sidebars = {
         "apps/autoplot_app/autoplot-app",
         "apps/recorder_app/recorder-app",
         "apps/file_manager_app/file-manager-app",
+        "apps/camera_app/camera-app",
       ],
     },
   ],
